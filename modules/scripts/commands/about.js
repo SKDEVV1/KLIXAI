9716c555ce67a4d9eead398996a811f1f664fddb
SKDEVV1 <sorianokhaile33@gmail.com>
2024-12-07 09:47:58 +0800

    about.js

--- modules/scripts/commands/about.js
+++ modules/scripts/commands/about.js
@@ -3,19 +3,23 @@ module.exports.config = {
   author: "Khaile",
   version: "1.0",
   category: "utility",
-  description: "information about the bot.",
+  description: "Information",
   adminOnly: false,
   usePrefix: true,
-  cooldown: 5
+  cooldown: 5,
 };
 
-module.exports.run = function ({ event, args }) {
-    const aboutMessage = `
-      **Bot Name:** [Klix Botify]
-      **Version:** [1.0]
-      **Description:** [Ai assistant that can help you]
-      **Developed by:** [Yan modified by kenlie]
-      **Contact:** [fb: https://www.facebook.com/localhostsoriano.8080]
-    `;
-api.sendMessage(message, event.sender.id);
-  };
+module.exports.run = async function ({ event, api, args }) {
+  const aboutMessage = `
+    **Bot Name:** Klix Botify
+    **Version:** 1.0
+    **Description:** AI assistant that can help you with tasks
+    **Developed by:** Yan, modified by Kenlie
+    **Contact:** [Facebook](https://www.facebook.com/localhostsoriano.8080)
+  `;
+  try {
+    api.sendMessage(aboutMessage, event.sender.id);
+  } catch (err) {
+    console.log(err);
+  }
+};
